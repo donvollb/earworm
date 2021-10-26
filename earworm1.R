1e8f1af8ca704a94d43e215979a2ad74ac85c9a6
donvollb <92096862+donvollb@users.noreply.github.com>
2021-10-26 09:10:27 +0200

    Add files via upload

---
+++ earworm1.R
@@ -0,0 +1,53 @@
+cat("Schneid dir die Haare
+Mach jetzt Ballett
+Schmeiß doch dein Studium
+Oder werd plötzlich fett
+Hass deinen Bruder
+Lieb die AfD
+Zieh doch nach Brooklyn
+Oder Fürstenwalde Spree
+Ist mir egal, mit wem du chillst
+Schlaf auf jedem Klo
+Mit jedem Typen, den du willst
+Mach mal dein Ding
+Mit Herz und Blut
+Mach dich glücklich
+Mach dich traurig
+Mach, mach, mach es gut
+Mach, was du willst
+Aber bring nie wieder
+Deinen neuen Freunde
+In meine Kneipe
+Kauf dir ein Auto
+Mach deinen Leberfleck weg
+Geh in die Kirche
+Fuck auf Aktien
+Fuck auf Crack
+Fang an zu boxen
+Schreib einen Roman
+Sprüh an jede Hauswand meiner Straße deinen Namen
+Ist mir egal, mit wem du chillst
+Schlaf auf jedem Klo
+Mit jedem Typen, den du willst
+Mach mal dein Ding
+Mit Herz und Blut
+Mach dich glücklich
+Mach dich traurig
+Mach, mach, mach es gut
+Mach, was du willst
+Aber bring nie wieder
+Deinen neuen Freunde
+In meine Kneipe
+Ist mir egal, mit wem du chillst
+Schlaf auf jedem Klo
+Mit jedem Typen, den du willst
+Mach mal dein Ding
+Mit Herz und Blut
+Mach dich glücklich
+Mach dich traurig
+Mach, mach, mach es gut
+Mach, was du willst
+Mach, was du willst
+Aber bring nie wieder
+Deinen neuen Freunde
+In meine Kneipe")
